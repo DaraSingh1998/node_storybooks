371c13e6c23e4c14743a56fc1d14461f3f3d43e1
Vibhor Bakshi <vibhor.bakshi1998@gmail.com>
2018-12-21 12:24:04 +0530

    Completed and Deploy ready

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
-module.exports={
-  mongoURI:'mongodb://Dara:Dragonballzgt1@ds131743.mlab.com:31743/storybooks--dev',
-  googleClientID:'734762023179-25leecjpfp73rgfaurk3pkfvgu65lqvh.apps.googleusercontent.com',
-  googleClientSecret:'zzpMXNRlJfVtLbvRhzu1G3Ht'
+if(process.env.NODE_ENV === 'production'){
+  module.exports=require('./keys_prod');
+}
+else {
+  module.exports=require('./keys_dev');
 }
